237f73e68f96d230233c49fbd34202e54ef4987b
Amir Sejdinovic <amir24V@yahoo.com>
2021-03-18 11:35:27 +0100

    Animating the buubles and colors

--- bubble.js
+++ bubble.js
@@ -6,7 +6,7 @@ document.body.prepend(canvas);
 const ctx = canvas.getContext('2d');
 
 const game = {req:''};
-const bubble = {bubbleCount:30,bubbles:[]};
+const bubble = {bubbleCount:30,speed:5,bubbles:[]};
 
 
 
@@ -15,12 +15,19 @@ const bubble = {bubbleCount:30,bubbles:[]};
 }*/
 
 function draw(){
+  ctx.clearRect(0,0,canvas.width,canvas.height);
   if(bubble.bubbles.length < bubble.bubbleCount){
     //Create new bubble
     bubbleMaker();
   }
-  bubble.bubbles.forEach((bubble, index)=>{
-    drawBubble(bubble.x,bubble.y,bubble.size);
+  bubble.bubbles.forEach((bub, index)=>{
+    bub.y-=bubble.speed;
+    bub.x-= Math.random()*5 -3;
+    if(bub.y < 0){
+      let temp = bubble.bubbles.splice(index,1);
+      console.log(temp);
+    }
+    drawBubble(bub.x,bub.y,bub.size,bub.color);
   });
   game.req = requestAnimationFrame(draw);
 }
@@ -28,22 +35,23 @@ function draw(){
 function bubbleMaker(){
   let bubbleSize = Math.random()*10+15;
   let xPos = Math.random() * (canvas.width - bubbleSize);
-  let yPos = Math.random() * (canvas.height - bubbleSize);
+  let yPos = Math.random() * (canvas.height - bubbleSize)+canvas.height;
   bubble.bubbles.push({
     x: xPos,
     y: yPos,
-    size: bubbleSize
+    size: bubbleSize,
+    color: [Math.random()*255,Math.random()*255,Math.random()*255]
 
   
   });
 }
 
-function drawBubble(xPos,yPos,bubbleSize){
+function drawBubble(xPos,yPos,bubbleSize,cl){
      
 
     const gradient = ctx.createRadialGradient(xPos,yPos-10,bubbleSize-15,xPos, yPos,bubbleSize+10);
 
-    gradient.addColorStop(0, 'rgba(0,0,255,0.9)');
+    gradient.addColorStop(0, 'rgba('+cl[0]+','+cl[1]+','+cl[2]+',0.9)');
     gradient.addColorStop(1, 'rgba(255,255,255,0.1)');
 
     
